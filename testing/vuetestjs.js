eabde1fbff1a5c49b55e5f459f21cd5620cc9f96
subham-chaudhary <shubhu.uwu@gmail.com>
2023-11-18 21:24:14 +0530

    vue js testing

---
+++ testing/vuetestjs.js
@@ -0,0 +1,17 @@
+// this is an option-api scripting style easy to understand and for small projects
+
+new Vue({
+    el: '#apps',
+    data: {
+        msgs: 'hello'
+    }
+    
+    
+    });
+    
+    new Vue({
+      el: '#app',
+      data: {
+        msg: 'bro'
+      }
+    });
